2de918b7f3d782e626922c08e3dda9685a40bc46
wxchit-95 <wangxc199596@gmail.com>
2020-10-01 22:46:17 -0400

    Update main.py
    backend changes (adding translation function)

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,33 @@
-from flask import Flask, redirect, url_for, request
-from transformers import pipeline
+from flask import Flask, redirect, url_for, request, render_template
+from transformers import AutoModelWithLMHead, AutoTokenizer
+import json
 
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
-
 @app.route('/result/translation/', methods = ['POST', 'GET'])
 def translators():
-    lang = request.form('language')
-    language_map = {"Germen":"de", "French":"fr", 'Roman':'ro'}
-    short_for_lang = langauge_map[lang]
-    lang_parameter = 'translation_en_to_' + short_for_lang
-    translation_result = pipeline(lang_parameter)
+    # Get the parameters and raw texts from the frontend side(JSON format).
+    request_json = request.get_json()
+    lang = request_json.get('language')
+    text = request_json.get('text')
+    
+    # Init model.
+    model = AutoModelWithLMHead.from_pretrained("t5-base")
+    tokenizer = AutoTokenizer.from_pretrained("t5-base")
+    
+    # Get translated result.
+    inputs = tokenizer.encode(
+        text.format(language = lang),
+        return_tensors="pt")
+    outputs = model.generate(inputs, max_length=40, num_beams=4, early_stopping=True)
+    result = tokenizer.decode(outputs[0])
+    response = {
+            "success": True,
+            "data": result
+            }
+    
+    return json.dumps(response), 200
+
 
 if __name__ == '__main__':
    app.run(debug = True)
