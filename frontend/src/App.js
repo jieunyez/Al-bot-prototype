55719883a87cae77b4e840438fca6bd97873fca2
JJ Zhang <jzhang40@terpmail.umd.edu>
2020-11-10 16:47:34 -0500

    Implement communication of user login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import { Input, Affix, Button } from 'antd';
 import {UserOutlined } from '@ant-design/icons'
 import Note from  './components/Note';
 import Writing from  './components/Writing';
+import axios from 'axios';
 import './App.css';
+
 
 function App() {
   const [fields, setFields] = useState([]);
   const [isNotes, setIsNotes] = useState([]);
   const [top, setTop] = useState(10);
+
+  const [id_input, setid] = useState(''); 
 
   function handleAddNote() {
     const cell_types = [...isNotes];
@@ -50,17 +54,41 @@ function App() {
     //handleNoteRm();
   }
 
+  function handleID(w) {
+    console.log(`writing ${w}`); 
+
+    const reqData= { ID: id_input}
+    // TO DO : change model 逻辑写在前端
+  
+    console.log(reqData)
+    //const response = await axios.post("http://0.0.0.0:5000/", reqData);
+    var config = { headers: {  
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'}
+      }
+
+    axios.post("http://127.0.0.1:5000/test",reqData, config).then(function (response) {
+      console.log('response is');
+      console.log(response);
+      console.log(response.data.data);
+      //setOutput(response.data.data);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
   return (
     <div className="App">
     <Affix offsetTop={top}>
       <Input
       placeholder="Enter your username here please"
       prefix={<UserOutlined className="site-form-item-icon" />}
+      onChange= {event => setid(event.target.value)}
         />
       <Input
       placeholder="Enter your password here please"
       />
-      <Button type="dashed" onClick={() => handleAddWrite()}>Submit Your Information</Button>
+      <Button type="primary" onClick={() => handleID(id_input)}>Sign In</Button>
         <Button type="dashed" onClick={() => handleAddWrite()}>+Write</Button>
         <Button type= "dashed" onClick={() => handleAddNote()}>+Note</Button>
     </Affix>
